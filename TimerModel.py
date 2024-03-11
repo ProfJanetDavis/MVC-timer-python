5f5488b70e6ddb25f2d2d7c40fdbcfcf0ef13525
Janet Davis <davisj@whitman.edu>
2024-03-11 15:41:38 -0700

    document all methods; remove initialTime

--- TimerModel.py
+++ TimerModel.py
@@ -3,17 +3,20 @@ import time
 from ObserverPattern import Subject, Observer
 
 class TimerModel(Subject):
-   """Implements a countdown timer."""
+   """Implements a countdown timer with a one-second resolution."""
 
    def __init__(self, seconds):
-       assert seconds > 0, "Initial time in seconds must be positive"
-       self.initialTime = seconds
-       self.currentTime = 0     # Invariant: 0 <= currentTime <= initialTime
+       """Initialize the timer with the given initial time in seconds."""
+       self.currentTime = 0     # Invariant: currentTime >= 0
        self.running = False     # True when thread is running
        self.thread = threading.Thread(target=self._thread_function) 
        self.observers = []
+       self.set_time(seconds)
 
    def _thread_function(self):
+       """Count down one second at a time.
+          Notify observers at each second and at the end of the timer.
+       """
        while self.running and self.currentTime > 0:
            time.sleep(1)
            self.currentTime -= 1
@@ -21,24 +24,33 @@ class TimerModel(Subject):
        self.running = False
        self.notify()
 
+   def set_time(self, seconds):
+       """Set both current and initial time to the given value of seconds."""
+       assert seconds > 0, "Time in seconds must be positive"
+       self.currentTime = seconds
+
    def start(self):
+       """Start the timer."""
        self.running = True
-       self.currentTime = self.initialTime
        self.thread.start()
        self.notify()
 
    def stop(self):
+       """Stop the timer."""
        self.running = False
        self.thread.join()
        self.notify()
    
    def attach(self, observer):
+       """Add an observer."""
        self.observers.append(observer)
 
    def detach(self, observer):
+       """Remove an observer."""
        self.observers.remove(observer)
 
    def notify(self):
+       """Call update on all observers."""
        for o in self.observers:
            o.update(self)
 
