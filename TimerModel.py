5be0bbe2314beeb91a9ffdb3dd7a9c3ec41e4368
Janet Davis <davisj@whitman.edu>
2024-03-11 15:16:29 -0700

    add timer model with a thread

---
+++ TimerModel.py
@@ -0,0 +1,31 @@
+import threading
+import time
+
+class TimerModel():
+   """Implements a countdown timer."""
+
+   def __init__(self, seconds):
+       self.startTime = seconds
+       self.currentTime = 0
+       self.running = False
+       self.thread = threading.Thread(target=self._thread_function) 
+
+   def _thread_function(self):
+       while self.currentTime > 0:
+           time.sleep(1)
+           self.currentTime -= 1
+           print(self.currentTime)
+
+   def start(self):
+       self.running = True
+       self.currentTime = self.startTime
+       self.thread.start()
+
+   def stop(self):
+       self.running = False
+       self.thread.join()
+
+if __name__ == '__main__':
+    timer = TimerModel(10)
+    timer.start()
+    print("Liftoff!")
