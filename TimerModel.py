17c1021a0d5331bbc8bdb1f8104559cfe1ed132d
Janet Davis <davisj@whitman.edu>
2024-03-11 22:18:37 -0700

    use properties

--- TimerModel.py
+++ TimerModel.py
@@ -12,60 +12,74 @@ class TimerModel(Subject):
 
    def __init__(self):
        """Initialize the timer."""
-       self.currentTime = 0     # Invariant: currentTime >= 0
-       self.running = False     # True when a thread is running
-       self.thread = None       
-       self.lock = Lock()
-       self.observers = []
+       self._time = 0            # Invariant: time >= 0
+       self._running = False     # True when a thread is running
+       self._thread = None       
+       self._lock = Lock()
+       self._observers = []
+
+   # See documentation for properties:
+   # https://realpython.com/python-getter-setter/#using-properties-instead-of-getters-and-setters-the-python-way
+
+   @property 
+   def time(self):
+       """Get current time in seconds."""
+       return self._time
+
+   @time.setter
+   def time(self, value):
+       """Set current time to the given non-negative value in seconds."""
+       assert value >= 0, "Time in seconds must be non-negative"
+       with self._lock:
+           self._time = value
+
+   @property 
+   def running(self):
+       """True if a thread is running and not about to stop."""
+       return self._running
 
    def _timer(self):
        """
        Count down one second at a time, stopping at zero.
-       This function should always be run in a new thread.
+       This function should always be run in a new _thread.
        """
-       while self.running and self.currentTime > 0:
+       while self._running and self._time > 0:
            self.notify()
            sleep(1)
-           with self.lock:
-               self.currentTime -= 1
-       if self.running:
-           self.running = False
+           with self._lock:
+               self._time -= 1
+       if self._running:
+           self._running = False
            self.notify()
-
-   def setTime(self, seconds):
-       """Set current time to the given value of seconds."""
-       assert seconds > 0, "Time in seconds must be positive"
-       with self.lock:
-           self.currentTime = seconds
 
    def startThread(self):
        """Start the timer from the current time."""
-       self.running = True
-       self.thread = Thread(target=self._timer) 
-       self.thread.start()
+       self._running = True
+       self._thread = Thread(target=self._timer) 
+       self._thread.start()
 
    def stopThread(self):
        """Stop the timer, retaining the current time."""
-       if self.running:
-           self.running = False
-           self.thread.join()
+       if self._running:
+           self._running = False
+           self._thread.join()
    
    def attach(self, observer):
        """Add an observer."""
-       self.observers.append(observer)
+       self._observers.append(observer)
 
    def detach(self, observer):
        """Remove an observer."""
-       self.observers.remove(observer)
+       self._observers.remove(observer)
 
    def notify(self):
        """Call update on all observers."""
-       for o in self.observers:
+       for o in self._observers:
            o.update(self)
 
 class TestObserver(Observer):
     def update(self, subject):
-        print(subject.currentTime)
+        print(subject._time)
 
 if __name__ == '__main__':
     myTimer = TimerModel()
@@ -77,13 +91,13 @@ if __name__ == '__main__':
     assert not myTimer.running
 
     print("Expected output: 10 9 8 7 6 5")
-    myTimer.setTime(10)
+    myTimer.time = 10
     myTimer.startThread()
     sleep(5.5)
     assert myTimer.running
 
     print("Expected output: 9 8")
-    myTimer.setTime(10)
+    myTimer.time = 10
     sleep(2)
     assert myTimer.running
     myTimer.stopThread()
@@ -94,7 +108,7 @@ if __name__ == '__main__':
     sleep(5)
 
     print("Expected output: 3 2 1 0")
-    myTimer.setTime(3)
+    myTimer.time = 3
     myTimer.startThread()
     assert myTimer.running
     sleep(5)
